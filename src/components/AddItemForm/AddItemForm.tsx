5e8e91f9ae96fe91e103b9a1e6ef9b5b013f2c4c
zemosamurai <zemosamurai@gmail.com>
2023-02-26 22:07:43 +0300

    add form from add task and todo

--- src/components/AddItemForm/AddItemForm.tsx
+++ src/components/AddItemForm/AddItemForm.tsx
@@ -1,22 +1,30 @@
 import React, {KeyboardEvent, useState} from 'react';
+import {Button} from "../../styles/components";
+import {ModalTodo} from "../ModalTodo/ModalTodo";
+import {AddItemFormWrapper, AddItemInput, ButtonAddItem, TitleAddItem} from "./styled/styles";
+import {theme} from "../../styles/theme";
 
 type AddItemForm = {
+    title: string
     addItem: (title: string) => void
+    disabled?: boolean
 }
 
-export const AddItemForm = ({addItem}: AddItemForm) => {
+
+export const AddItemForm = ({addItem, title, disabled}: AddItemForm) => {
     const [value, setValue] = useState<string>('')
     const [error, setError] = useState(false)
+    const [modalActive, setModalActive] = useState(false)
 
     const onAddItem = () => {
         if (value.trim() !== '') {
             addItem(value.trim())
             setValue('')
+            setModalActive(false)
         } else {
             setError(true)
         }
     }
-
     const onEnterAddItem = (e: KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter') {
             onAddItem()
@@ -26,11 +34,32 @@ export const AddItemForm = ({addItem}: AddItemForm) => {
 
     }
 
+    const closeModal = () => setModalActive(false)
+    const openModal = () => setModalActive(true)
+
     return (
-        <div>
-            <input value={value} onChange={(e) => setValue(e.currentTarget.value)} onKeyDown={onEnterAddItem}/>
-            <button onClick={onAddItem} >+</button>
-            {error && <div>Error</div>}
-        </div>
+        <>
+            <ButtonAddItem disabled={disabled} onClick={openModal}>
+                + {title}
+            </ButtonAddItem>
+            <div>
+                {modalActive && <ModalTodo active={modalActive} setActive={closeModal}>
+                    <AddItemFormWrapper direction={'column'}>
+                        <TitleAddItem size={'20px'} color={theme.colors.fontPrimary}>Add {title}</TitleAddItem>
+
+                        <AddItemInput
+                            value={value}
+                            onChange={(e) => setValue(e.currentTarget.value)}
+                            onKeyDown={onEnterAddItem}
+                            placeholder={'Title'}
+                        />
+
+                        <Button bg={theme.colors.primary} onClick={onAddItem}>Add</Button>
+                        {error && <div>Error</div>}
+                    </AddItemFormWrapper>
+                </ModalTodo>}
+            </div>
+        </>
+
     )
 }
